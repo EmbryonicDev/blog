bc8a8026ae8d695e7c860f03798b90083f286868
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-20 18:23:06 +0200

    Create Blog: Create view BlogCreateView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class BlogListView(ListView):
@@ -10,3 +10,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
