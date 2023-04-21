a983018165ff7287a223b5e3b5245c0a6a072e75
djdevlam-apple <embryonic.dev@gmail.com>
2023-04-21 15:59:00 +0200

    Add def test_post_creatview

--- blog/tests.py
+++ blog/tests.py
@@ -46,3 +46,16 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New Title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New Title")
+        self.assertEqual(Post.objects.last().body, "New text")
